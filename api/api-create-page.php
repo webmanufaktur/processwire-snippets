2843f7cf4b63010e69eae480469b0a602d01fece
User <dev@development.org>
2018-06-04 11:59:59 +0200

    Create page via API

---
+++ api/api-create-page.php
@@ -0,0 +1,25 @@
+<?php
+
+// Create new page via API
+$newpage = new Page();
+
+//http://cheatsheet.processwire.com/page/built-in-methods-reference/page-setoutputformatting-true-false/
+$newpage->setOutputFormatting(false);
+
+// example template
+$newpage->template = 'yourTemplate';
+
+// example parent
+$newpage->parent = wire('pages')->get('/parent/'); 
+
+// example name - leave empty and let ProcessWire create a name for the page based on title
+$newpage->name = "foo";
+
+//example title
+$newpage->title = "My API-generated new PW page";
+
+// example field with example value
+$newpage->fieldname = "my field value"; 
+
+// save page
+$newpage->save();
