1204f12ba69b7ad3f1135d83060cbcc552736125
webmanufaktur <info@webmanufaktur.net>
2018-06-04 13:05:16 +0200

    Log all outgoing emails

--- hooks-ready-php/ready-log-outgoing-emails.php
+++ hooks-ready-php/ready-log-outgoing-emails.php
@@ -1,22 +1,22 @@
 <?php
 
-// log all outgoing emails
+// Log all outgoing emails
 // https://processwire-recipes.com/recipes/logging-outgoing-emails/
 
 $wire->addHookAfter('WireMail::send', function($event) {
-	
+
 	$mail = $event->object;
-	
+
 	$event->wire('log')->save('sent-mail',
-	
+
 		"SENT ($event->return of " . count($mail->to) . "), " .
-		
+
 		"SUBJECT ($mail->subject), " .
-		
+
 		"TO (" .  implode(", ", $mail->to) . "), " .
-		
+
 		"FROM ($mail->from)"
-		
+
 	);
-	
+
 });
