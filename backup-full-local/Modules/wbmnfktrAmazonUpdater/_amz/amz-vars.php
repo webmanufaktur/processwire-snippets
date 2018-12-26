0fdbb7e0465d0b240aa6d7df158e219ba1bf6cff
webmanufaktur <info@webmanufaktur.net>
2018-12-26 21:50:22 +0100

    Fix

--- backup-full-local/Modules/wbmnfktrAmazonUpdater/_amz/amz-vars.php
+++ backup-full-local/Modules/wbmnfktrAmazonUpdater/_amz/amz-vars.php
@@ -1,6 +1,6 @@
 <?php
 
 //Amazon-Keys und Affiliate-ID
-define('AWS_API_KEY', 'AKIAJVCHRK3A6NFTWWOQ');
-define('AWS_API_SECRET_KEY', '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');
-define('AWS_ASSOCIATE_TAG', 'hommageuhren-21');
+define('AWS_API_KEY', 'KEY');
+define('AWS_API_SECRET_KEY', 'SECRET');
+define('AWS_ASSOCIATE_TAG', 'tag-20');
