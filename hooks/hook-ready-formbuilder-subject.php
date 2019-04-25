b41a61373cc9d8e33a923d7ccbf719faf6687707
webmanufaktur <info@webmanufaktur.net>
2019-04-25 18:35:13 +0200

    Create hook-ready-formbuilder-subject.php

---
+++ hooks/hook-ready-formbuilder-subject.php
@@ -0,0 +1,16 @@
+<?php namespace ProcessWire;
+
+// Purpose: change FormBuilder subject, add details from user input
+// Location: ready.php
+
+$forms->addHookBefore('FormBuilderProcessor::emailForm', function($event) {
+
+	$form = $event->object;
+    $formData = $event->arguments(0);
+
+    if($form->name == "yourFormNameHere") {
+        $subjectField = $formData->getChildByName('yourSubjectField')->value;
+        $form->emailSubject = $form->emailSubject . " (" . $subjectField . ")";
+    }
+    
+});
