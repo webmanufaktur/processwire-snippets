67c0cc9f892153c4650177818600ed0e7a3cf225
webmanufaktur <info@webmanufaktur.net>
2019-04-25 18:35:10 +0200

    Create hook-ready-formbuilder-from.php

---
+++ hooks/hook-ready-formbuilder-from.php
@@ -0,0 +1,16 @@
+<?php namespace ProcessWire;
+
+// Purpose: change FormBuilder from address to user input
+// Location: ready.php
+
+$forms->addHookBefore('FormBuilderProcessor::emailForm', function($event) {
+
+    $form = $event->object;
+    $formData = $event->arguments(0);
+
+    if($form->name == "yourFormNameHere") {
+        $fakefrom = $formData->getChildByName('yourEmailFieldHere')->value;
+        $form->emailFrom = $fakefrom;
+    }
+    
+});
