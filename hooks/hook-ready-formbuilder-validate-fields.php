97e84206d87aafbf9241b079c253055a202fedb1
webmanufaktur <info@webmanufaktur.net>
2019-04-25 18:38:31 +0200

    Create hook-ready-formbuilder-validate-fields.php

---
+++ hooks/hook-ready-formbuilder-validate-fields.php
@@ -0,0 +1,28 @@
+<?php
+
+// Purpose: validate or compare FormBuilder fields
+// Location: ready.php
+
+wire()->addHookBefore("FormBuilderProcessor::processInputDone", function($event) {
+    
+    $form  = $event->arguments(0);
+
+    if($form->name == 'yourFormName') {
+        
+        $field1 = $form->getChildByName('your_first_field');
+        $field2 = $form->getChildByName('your_second_field');
+        
+        $val1 = $field1->value;
+        $val2 = $field2->value;
+        
+        // check, validate, do whatever you want
+        if($val1 > $val2) {
+            $field1->error("Error message field1.");
+            $field2->error("Error message field2.");
+        }
+
+    }
+
+});
+
+
